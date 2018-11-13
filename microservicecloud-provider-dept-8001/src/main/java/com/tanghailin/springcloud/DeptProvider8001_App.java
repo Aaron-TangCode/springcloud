eb70204288c2453a8e2b9d91b657c66571fff656
Aaron <1025741620@qq.com>
2018-11-13 14:33:54 +0800

    主机映射名称修改

--- microservicecloud-provider-dept-8001/src/main/java/com/tanghailin/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/tanghailin/springcloud/DeptProvider8001_App.java
@@ -2,11 +2,12 @@ package com.tanghailin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DeptProvider8001_App.class, args);
 	}
-
 }
