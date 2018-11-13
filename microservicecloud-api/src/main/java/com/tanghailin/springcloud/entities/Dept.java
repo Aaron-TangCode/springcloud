ea7201f645c39cf3a26f45685a8eb97657d87089
Aaron <1025741620@qq.com>
2018-11-13 10:13:38 +0800

    微服务基本框架

---
+++ microservicecloud-api/src/main/java/com/tanghailin/springcloud/entities/Dept.java
@@ -0,0 +1,44 @@
+package com.tanghailin.springcloud.entities;
+
+import java.io.Serializable;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//@SuppressWarnings("serial")
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@Data
+public class Dept implements Serializable {
+	private Long deptno;
+	private String dname;
+	private String db_source;
+	
+	public Long getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(Long deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getDb_source() {
+		return db_source;
+	}
+
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
+	}
+
+	public static void main(String[] args) {
+		
+	}
+}
