4ea34df059a55e55acbecad0e162ec658e652993
Aaron <1025741620@qq.com>
2018-11-24 16:10:04 +0800

    ribbon的核心组件IRule（负载均衡算法选择）

--- microservicecloud-consumer-dept-80/src/main/java/com/tanghailin/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/tanghailin/springcloud/cfgbeans/ConfigBean.java
@@ -5,11 +5,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
-@Configuration
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
+@Configuration//加了这个@Configuration，这个类就是applicationContext.xml
 public class ConfigBean {
 	@Bean
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+//	@Bean//负载均衡（随机算法）
+//	public IRule getRule() {
+//		return new RandomRule();
+//	}
+	@Bean//负载均衡（轮询算法）
+	public IRule getRule2() {
+		return new RoundRobinRule();
 	}
 }
