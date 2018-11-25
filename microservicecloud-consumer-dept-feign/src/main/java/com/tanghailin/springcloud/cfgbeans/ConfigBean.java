dfcfc15af939c87132da6c506b1fba30df94806b
Aaron <1025741620@qq.com>
2018-11-25 11:38:51 +0800

    Feign负载均衡

---
+++ microservicecloud-consumer-dept-feign/src/main/java/com/tanghailin/springcloud/cfgbeans/ConfigBean.java
@@ -0,0 +1,28 @@
+package com.tanghailin.springcloud.cfgbeans;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+
+@Configuration//加了这个@Configuration，这个类就是applicationContext.xml
+public class ConfigBean {
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+	
+//	@Bean//负载均衡（随机算法）
+//	public IRule getRule() {
+//		return new RandomRule();
+//	}
+//	@Bean//负载均衡（轮询算法）
+//	public IRule getRule2() {
+//		return new RoundRobinRule();
+//	}
+}
